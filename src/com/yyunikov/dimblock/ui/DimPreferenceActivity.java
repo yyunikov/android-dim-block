e45b49e6fda137136be9a5553598f08c85a64fda
Yuriy Yunikov <yyunikov@yyunikov-PC.(none)>
2013-12-20 00:24:14 +0200

    Naming changes.

--- src/com/yyunikov/dimblock/ui/DimPreferenceActivity.java
+++ src/com/yyunikov/dimblock/ui/DimPreferenceActivity.java
@@ -1,16 +1,16 @@
 package com.yyunikov.dimblock.ui;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import com.yyunikov.dimblock.R;
 
 /**
  * Author: yyunikov
  * Date: 12/19/13
  */
-public class DimPreferences extends Activity {
+public class DimPreferenceActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dim_preferences_layout);
+        setContentView(R.layout.activity_preference_dim);
     }
 }
