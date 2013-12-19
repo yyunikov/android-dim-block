01b812bd072cc2584afc5a4057579742ae374011
Yuriy Yunikov <yyunikov@yyunikov-PC.(none)>
2013-12-19 23:56:31 +0200

    Added empty project.

---
+++ src/com/yyunikov/dimblock/ui/DimPreferences.java
@@ -0,0 +1,16 @@
+package com.yyunikov.dimblock.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.yyunikov.dimblock.R;
+
+/**
+ * Author: yyunikov
+ * Date: 12/19/13
+ */
+public class DimPreferences extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dim_preferences_layout);
+    }
+}
