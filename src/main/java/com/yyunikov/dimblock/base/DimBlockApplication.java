0ad30ac199322f5b32807310fc60e05b54b977d7
Yuriy Yunikov <yyunikov@yyunikov-PC.(none)>
2014-01-27 23:15:22 +0200

    Added Google Analytics and Application class for it.

--- src/main/java/com/yyunikov/dimblock/base/DimBlockApplication.java
+++ src/main/java/com/yyunikov/dimblock/base/DimBlockApplication.java
@@ -1,8 +1,52 @@
+/*
+ * Copyright (c) 2014 Yuriy Yunikov
+ */
+
 package com.yyunikov.dimblock.base;
+
+import android.app.Application;
+
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
 
 /**
  * Author: yyunikov
  * Date: 1/27/14
  */
-public class DimBlockApplication {
+public class DimBlockApplication extends Application {
+
+    private static final String GA_TRACKING_ID = "UA-41878921-2";
+
+    // Prevent hits from being sent to reports, i.e. during testing.
+    // TODO remove
+    private static final boolean GA_IS_DRY_RUN = false;
+
+    private static GoogleAnalytics mGa;
+    private static Tracker mTracker;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mGa = GoogleAnalytics.getInstance(this);
+        mTracker = mGa.getTracker(GA_TRACKING_ID);
+
+        mGa.setDryRun(GA_IS_DRY_RUN);
+        // TODO remove
+        mGa.getLogger().setLogLevel(com.google.analytics.tracking.android.Logger.LogLevel.VERBOSE);
+    }
+
+    /*
+    * Returns the Google Analytics tracker.
+    */
+    public static Tracker getGaTracker() {
+        return mTracker;
+    }
+
+    /*
+    * Returns the Google Analytics instance.
+    */
+    public static GoogleAnalytics getGaInstance() {
+        return mGa;
+    }
 }
